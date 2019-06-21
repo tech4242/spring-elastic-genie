3697b2fb8d20af0ec86e47eed3b5d6df839e076a
Nikolay Dimolarov <dimolarov@hotmail.com>
2019-06-21 10:26:56 +0200

    fixed docker-compose, added new Model, added postgres to application.properties and gradle

--- src/main/java/springelasticgenie/controller/MainController.java
+++ src/main/java/springelasticgenie/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
+public class MainController {
 
     @RequestMapping("/")
     public String index() {
---
+++ src/main/java/springelasticgenie/model/Movie.java
@@ -0,0 +1,31 @@
+package springelasticgenie.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
